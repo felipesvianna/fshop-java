ce6d36d5d9a4339aafda87a88b7753754eecc146
Felipe <felipe.vianna10@gmail.com>
2022-02-06 19:34:36 -0300

    Add test configuration

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  let wrapper;
+
+  it('should render withour errors', () => {
+    wrapper = shallow(<App/>);
+    expect(wrapper.text()).toEqual('F-Shop');
+  });
 });
+
+
